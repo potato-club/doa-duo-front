b3658c61d4d5703b81de2bc47b16bbd645a1d52e
CHO YONGSU <choyongsu2001@naver.com>
2024-07-22 13:57:48 +0900

    dsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+    <div>dsa</div>
+    <div>dsa</div>
+    <div>dsa</div>
+    <div>dsa</div>
+    <div>dsa</div>
+    </Layout>
   );
 }
 
 export default App;
+
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  max-width: 500px;
+  @media screen and (max-width: 425px) {
+    width: 425px;
+  }
+`;
+
