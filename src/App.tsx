061f90c05c97d82f9064cc049e928dcfb61d7a60
CHO YONGSU <choyongsu2001@naver.com>
2024-07-22 21:34:02 +0900

    dsa

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,26 @@ import { RecoilRoot } from "recoil";
 import SignUp from "./pages/LoginPage/SignUp";
 import { ResponserMainPage } from "./pages/Responser/ResponserMainPage";
 import MatchingCompletedModal from "./components/Modal/MatchingCompletedModal";
+import Start from "./pages/LoginPage/Start";
+import useWindowSize from "./components/UsewindowSize";
+
+interface LayoutProps {
+  width:number,
+}
 
 function App() {
+
+
+  const { width, height } = useWindowSize();
+
   return (
     <RecoilRoot>
-      <Layout>
-        <MatchingCompletedModal username="1" />
+      <Layout width={width}>
         <Routes>
+        <Route path="/" element={<Start />} />
           <Route path="/login" element={<Login />} />
-
           <Route path="/signup" element={<SignUp />} />
           <Route path="/main" element={<Login />} />
-
           <Route path="/res-main" element={<ResponserMainPage />} />
         </Routes>
       </Layout>
@@ -26,7 +34,7 @@ function App() {
 
 export default App;
 
-const Layout = styled.div`
+const Layout = styled.div<LayoutProps>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -35,6 +43,6 @@ const Layout = styled.div`
   width: 100%;
   height: 100%;
   @media (max-width: 425px) {
-    width: 425px;
+    width: ${props => props.width}px;
   }
 `;
