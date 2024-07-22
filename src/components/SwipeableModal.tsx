c6496dae122b23905be1d669b56330a86dfc0bd7
Pdom <pdom0327@gmail.com>
2024-07-23 05:01:29 +0900

    ㅁㄴㅇ

--- src/components/SwipeableModal.tsx
+++ src/components/SwipeableModal.tsx
@@ -9,13 +9,11 @@ const ModalBackdrop = styled.div<{ isOpen: boolean }>`
   left: 0;
   right: 0;
   bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: flex-end;
   justify-content: center;
-  transition: opacity 0.3s;
-  opacity: ${(props) => (props.isOpen ? 1 : 0)};
-  pointer-events: ${(props) => (props.isOpen ? 'auto' : 'none')};
+  transition: background-color 0.3s;
+  pointer-events: none;
   z-index: 1000;
 `;
 
@@ -26,6 +24,7 @@ const ModalContent = styled(animated.div)`
   border-radius: 20px 20px 0 0;
   box-shadow: 0px -5px 15px rgba(0, 0, 0, 0.2);
   position: relative;
+  pointer-events: auto;
 `;
 
 const DragHandle = styled.div`
@@ -42,29 +41,39 @@ const DragHandle = styled.div`
 
 interface SwipeableModalProps {
   isOpen: boolean;
+  onOpen: () => void;
   onClose: () => void;
   children?: React.ReactNode;
   bottomOffset?: number;
 }
 
+const OFFSET = 500;
+
 export const SwipeableModal: React.FC<SwipeableModalProps> = ({
   isOpen,
+  onOpen,
   onClose,
   children,
   bottomOffset,
 }) => {
   const [{ y }, api] = useSpring(() => ({ y: window.innerHeight }));
 
-  const openModal = useCallback(() => {
-    api.start({ y: 0 });
-  }, [api]);
+  const openModal = useCallback(
+    (velocity = 0) => {
+      api.start({ y: 0, config: { velocity } });
+    },
+    [api]
+  );
 
-  const closeModal = useCallback((velocity = 0) => {
-    api.start({
-      y: window.innerHeight,
-      config: { velocity },
-    });
-  }, [api]);
+  const closeModal = useCallback(
+    (velocity = 0) => {
+      api.start({
+        y: window.innerHeight - OFFSET,
+        config: { velocity },
+      });
+    },
+    [api]
+  );
 
   const handleDrag = useDrag(
     (state) => {
@@ -75,15 +84,28 @@ export const SwipeableModal: React.FC<SwipeableModalProps> = ({
 
       if (my < -70) cancel && cancel();
 
-      if (last) {
-        if (my > modalHeight / 6 || velocity > 0.5) {
-          closeModal(velocity);
-          onClose();
+      if (isOpen) {
+        if (last) {
+          if (my > modalHeight / 6 || velocity > 0.5) {
+            closeModal(velocity);
+            onClose();
+          } else {
+            openModal();
+          }
         } else {
-          openModal();
+          api.start({ y: my, immediate: true });
         }
       } else {
-        api.start({ y: my, immediate: true });
+        if (last) {
+          if (my < (modalHeight * 5) / 6 || velocity > 0.5) {
+            openModal(velocity);
+            onOpen();
+          } else {
+            closeModal();
+          }
+        } else {
+          api.start({ y: window.innerHeight - OFFSET + my, immediate: true });
+        }
       }
     },
     { from: () => [0, y.get()], bounds: { top: 0 }, rubberband: true }
