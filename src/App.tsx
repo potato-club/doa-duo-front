24d9f470791287a33cc3940659b0a543e9de7923
CHO YONGSU <choyongsu2001@naver.com>
2024-07-22 17:48:18 +0900

    dsa

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 import Login from './pages/LoginPage/Login';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-
+import SignUp from './pages/LoginPage/SignUp';
 function App() {
   return (
     <RecoilRoot>
       <Layout>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/main" element={<Login />} />
         </Routes>
       </Layout>
@@ -23,4 +24,7 @@ const Layout = styled.div`
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
+  position: relative;
+  
+ width: 100%
 `;
